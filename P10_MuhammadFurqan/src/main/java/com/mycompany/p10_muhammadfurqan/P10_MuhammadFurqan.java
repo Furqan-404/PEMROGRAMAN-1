b39ccbbc4e8dfebf24c922254f34abbae1a6a850
Furqan2.0 <muhammadfurqan072005@gmail.com>
2026-05-05 19:52:15 +0700

    Add files via upload

---
+++ P10_MuhammadFurqan/src/main/java/com/mycompany/p10_muhammadfurqan/P10_MuhammadFurqan.java
@@ -0,0 +1,17 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p10_muhammadfurqan;
+
+/**
+ *
+ * @author muham
+ */
+public class P10_MuhammadFurqan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
